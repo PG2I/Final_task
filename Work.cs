a4edaa15964b78474ccf28efad7bd1ff323d48c2
«Pavel» <paveligorevich2108@gmail.com>
2023-11-09 23:47:48 +0200

    to push

--- Work.cs
+++ Work.cs
@@ -7,7 +7,7 @@
 
     static void Mass()
     {
-        Console.Write("Введите количество строк в массиве: ");
+        Console.Write("Введите количество строк в массиве : ");
         int n = int.Parse(Console.ReadLine()!);
 
         string[] words = new string[n];
@@ -18,7 +18,7 @@
             words[i] = Console.ReadLine()!;
         }
 
-        Console.WriteLine("\nИсходный массив строк:");
+        Console.WriteLine("\nИсходный массив строк :");
         foreach (string word in words)
         {
             Console.WriteLine(word);
@@ -26,7 +26,7 @@
 
         string[] filteredWords = FilterWords(words);
 
-        Console.WriteLine("\nМассив коротких строк (длина <= 3 символов):");
+        Console.WriteLine("\nМассив коротких строк (длина <= 3 символов) :");
         foreach (string word in filteredWords)
         {
             Console.WriteLine(word);
@@ -57,5 +57,6 @@
         }
 
         return filteredWords;
+
     }
 }
